e1cffbb550293fa74b2bf9d8400570517604fa05
Madaraka02 <victormadaraka02@gmail.com>
2022-02-03 22:43:58 +0300

    first commit

---
+++ Api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.FileField(null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_published = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL)
+class PostImages(models.Model):
+    pass
