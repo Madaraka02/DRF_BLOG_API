6b06edd67f798b2cea3e21ab14d84c8b89120de3
Madaraka02 <victormadaraka02@gmail.com>
2022-02-04 10:38:09 +0300

    added knox logout

--- Blog/settings.py
+++ Blog/settings.py
@@ -116,4 +116,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import *
+from knox import views as knox_views
 
 
 urlpatterns = [
     path('login/', loginView),
     path('register/', registerView),
+    path('logout/', knox_views.LogoutView.as_view()),
+    path('logoutall/', knox_views.LogoutAllView.as_view()),
 ]
